5e1be862d4450326f9c60e1b1312e8ab886cd76d
Clément Hamada <clementhamada@protonmail.com>
2021-03-10 16:31:01 +0100

    stack: Format stack_load.c

--- src/stack_load.c
+++ src/stack_load.c
@@ -2,46 +2,44 @@
 
 t_stack_val     stack_eval(char const **str)
 {
-        t_ustack_val    value;
-        t_stack_val             sign;
+	t_ustack_val	value;
+	t_stack_val		sign;
 
-        value = 0;
-        sign = 1;
-        while (**str == '-' || **str == '+')
-        {
-                if (*(*str++) == '-')
-                        sign *= -1;
-        }
-        while (**str && '0' <= **str && **str <= '9' && value < STACK_VAL_MAX)
-                value = value * 10 + *(*str)++ - '0';
-        return (value);
+	value = 0;
+	sign = 1;
+	while (**str == '-' || **str == '+')
+		if (*(*str++) == '-')
+			sign *= -1;
+	while (**str && '0' <= **str && **str <= '9' && value < STACK_VAL_MAX)
+		value = value * 10 + *(*str)++ - '0';
+	return (value);
 }
 
 t_stack *stack_load(char const **values)
 {
-        char const              *current;
-        t_ustack_val    value;
-        t_stack                 *new_elem;
-        t_stack                 *stack;
+	char const		*current;
+	t_ustack_val	value;
+	t_stack			*new_elem;
+	t_stack			*stack;
 
-        stack = NULL;
-        while ((current = *values))
-        {
-                value = stack_eval(&current);
-                if (*current)
-                {
-                        dprintf(2, "stack_load: Invalid argument: '%s'\n", current);
-                        stack_clear(&stack);
-                        return (stack);
-                }
-                if (!(new_elem = stack_new(value)))
-                {
-                        perror("stack_new");
-                        stack_clear(&stack);
-                        return (stack);
-                }
-                stack_push_back(&stack, new_elem);
-                values++;
-        }
-        return (stack);
+	stack = NULL;
+	while ((current = *values))
+	{
+		value = stack_eval(&current);
+		if (*current)
+		{
+			dprintf(2, "stack_load: Invalid argument: '%s'\n", current);
+			stack_clear(&stack);
+			return (stack);
+		}
+		if (!(new_elem = stack_new(value)))
+		{
+			perror("stack_new");
+			stack_clear(&stack);
+			return (stack);
+		}
+		stack_push_back(&stack, new_elem);
+		values++;
+	}
+	return (stack);
 }
